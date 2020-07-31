c4fb4cfdf5bd46d3c154c35c78c93c8d3fc614dc
Erez Binyamin <exb4096@rit.edu>
2020-07-31 13:30:52 -0400

    Corrected type local.sh -> game.sh in server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f /tmp/f
 mkfifo /tmp/f
-cat /tmp/f | /bin/bash local.sh 2>&1 | nc -l ${1:-1234} | tee -a /tmp/f
+cat /tmp/f | /bin/bash game.sh 2>&1 | nc -l ${1:-1234} | tee -a /tmp/f
