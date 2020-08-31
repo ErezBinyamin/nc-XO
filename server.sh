fc353c305baee6c8183f1893544b2ccd27de1345
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-08-31 17:05:00 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#remove any existing fif's with these names
 rm -f /tmp/in /tmp/out
-mkfifo /tmp/in
-mkfifo /tmp/out
+#create fifos with these names
+mkfifo /tmp/in		#ie the command-in stream
+mkfifo /tmp/out	        #output from the given program
 
 cat /tmp/in | /bin/bash game.sh 2>&1 | tee -a /tmp/out | nc -l ${1:-1234} > /tmp/in &
 cat /tmp/out &
